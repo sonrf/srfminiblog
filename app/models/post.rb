67728b61c3cfa3c22f6c3f9d4e4601d9de86b591
Sonia Rodriguez <sonrf@hotmail.com>
2013-03-22 18:01:04 +0000

    Active Record

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+	attr_accessible :body, :title, :category
+
+	validates :title, :category, :presence => true
+	validates :title, :uniqueness => true
+
+	before_validation :apply_title_format
+
+private
+	
+	def apply_title_format
+		self[:title] = self[:title].strip.capitalize
+		
+	end
+
+	
 end
