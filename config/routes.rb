4ec6c8f50081e07946e9d5d80cde41765824c4b0
Sonia Rodriguez <sonrf@hotmail.com>
2013-04-02 10:51:56 +0100

    Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 MiniBlog::Application.routes.draw do
-  resources :posts
+
+  resources :posts do
+  resources :comments
+  end
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -57,4 +61,5 @@ MiniBlog::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
